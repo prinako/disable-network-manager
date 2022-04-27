036bebfd19c65b7ed9db900fa26edd1bae3b2220
Prince Nyarko <nyankonton122@gmail.com>
2022-04-27 09:21:24 -0300

    fg

--- disable.sh
+++ disable.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
-
-echo "qual é o nome da rede?"
+# Leia a entrada do usuário
+echo "qual é o nome do dispositivo de rede (tipo enp0s...)?"
 read nome
 
-echo "qual é IP com /máscara?"
+echo "qual é IP com /máscara (IP/24 ou IP/8)?"
 read IP
 
 echo "qual é o gateway?"
@@ -12,31 +12,39 @@ read gateway
 echo "qual é o DNS?"
 read DNS
 
+#Crie o arquivo em '/etc/systemd/network/' com o nome 20-wired.network e copie as informações que o usuário digitou
 echo "[Match]" > /etc/systemd/network/20-wired.network
-echo "Name=$nome" > /etc/systemd/network/20-wired.network
+echo "Name=$nome" >> /etc/systemd/network/20-wired.network
 
-echo > /etc/systemd/network/20-wired.network
-echo "[Network]" > /etc/systemd/network/20-wired.network
+echo >> /etc/systemd/network/20-wired.network
+echo "[Network]" >> /etc/systemd/network/20-wired.network
 
-echo "Address=$IP" > /etc/systemd/network/20-wired.network
-echo "Gateway=$gateway" > /etc/systemd/network/20-wired.network
-echo "DNS=$DNS" > /etc/systemd/network/20-wired.network
+echo "Address=$IP" >> /etc/systemd/network/20-wired.network
+echo "Gateway=$gateway" >> /etc/systemd/network/20-wired.network
+echo "DNS=$DNS" >> /etc/systemd/network/20-wired.network
 
+#Para parar o serviço do NetworkManager
 systemctl stop NetworkManager
+
+#Para iniciar o serviço do systemd-networkd
 systemctl start systemd-networkd
 
+#Para desabilitar o serviço do NetworkManager e ativar o systemd-networkd
 systemctl disable NetworkManager
 systemctl enable systemd-networkd
 
-echo "seu sistema dasea on que?"
+#Para remover o serviço do NetworkManager
+echo "Seu sistema se baseia em que?"
 echo "1 Arch"
-echo "2 Debin (Ubuntu, linux Mint)"
-read qual-sistema
+echo "2 Debian (Ubuntu, linux Mint)"
+read qualsistema
 
-if [ $qual-sistema == 1  ]; then
+if [ $qualsistema == 1  ]; then
+# sistemas operacionais baseados no Arch Linux
      pacman -Rc network-manager-applet
-else
-     apt-get uninstall network-manager-applet
+else if [ $]
+ sistemas operacionais baseados no Debian (Ubuntu, Mint)
+     apt-get remove network-manager
 fi
 
 echo "Reinicie a máqina"
